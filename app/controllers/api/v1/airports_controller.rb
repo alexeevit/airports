3346c9636a91310fdf9eabba5257b55834f66f73
Vyacheslav Alexeev <alexeev.corp@gmail.com>
2021-11-19 12:59:54 +0800

    countries filtering is added

--- app/controllers/api/v1/airports_controller.rb
+++ app/controllers/api/v1/airports_controller.rb
@@ -2,8 +2,18 @@ module API
   module V1
     class AirportsController < BaseController
       def index
-        airports = Airport.all
+        airports = Airport.where(filter_params).all
         render json: AirportBlueprint.render(airports)
+      end
+
+      private
+
+      def filter_params
+        permitted_params = params.permit(countries: [])
+
+        {}.tap do |hash|
+          hash[:country_alpha2] = permitted_params[:countries] if permitted_params[:countries].present?
+        end
       end
     end
   end
--- spec/api/v1/airports_spec.rb
+++ spec/api/v1/airports_spec.rb
@@ -2,17 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'API::V1::AirportsController', type: :request do
   describe 'show' do
-    subject(:airports_request) { json_get api_v1_airports_url}
+    subject(:airports_request) { ->(params = {}) { json_get api_v1_airports_url, params: params } }
 
     before do
       %i[fra muc zrh vie inn grz].each { |iata| create(:airport, iata) }
     end
 
     it 'returns airports', :aggregate_failures do
-      airports_request
+      airports_request.call
 
       expect(response.status).to eq(200)
       expect(json.size).to eq(6)
+    end
+
+    it 'returns airports for specific countries' do
+      airports_request.call(countries: ['AT', 'CH'])
+
+      expect(response.status).to eq(200)
+      expect(json.size).to eq(4)
     end
   end
 end
